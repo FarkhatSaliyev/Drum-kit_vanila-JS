d4d59fc4fd5fcaa3acf93b6daedc94386bc0d046
Farkhat Saliyev <saliyev.farkhat@gmail.com>
2021-01-31 19:00:51 -0800

    Drum Kit_Vanila JS

---
+++ index.js
@@ -0,0 +1,58 @@
+const btns = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < btns; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", () => {
+    const btnInnerText = this.innerHTML;
+
+    makeSound(btnInnerText);
+    btnPressed(btnInnerText);
+  });
+}
+
+document.addEventListener("keypress", (e) => {
+  makeSound(e.key);
+  btnPressed(e.key);
+});
+
+const makeSound = (key) => {
+  switch (key) {
+    case "a":
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "s":
+      let kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "x":
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case " ":
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "k":
+      let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "l":
+      let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "m":
+      let tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    default:
+      console.log(key);
+  }
+};
+
+const btnPressed = (currentKey) => {
+  const pressedBtn = document.querySelector(`.${currentKey}`);
+  pressedBtn.classList.add("pressed");
+  setTimeout(() => {
+    pressedBtn.classList.remove("pressed");
+  }, 100);
+};
